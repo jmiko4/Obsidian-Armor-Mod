110bd066ff756a1afd32dea8941ef5ab4b21d4c3
jmiko4 <jmiko4@byui.edu>
2023-01-31 13:38:58 -0700

    Initial commit

---
+++ src/main/java/net/justin/obsidianarmor/ObsidianArmor.java
@@ -0,0 +1,18 @@
+package net.justin.obsidianarmor;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ObsidianArmor implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final String MOD_ID = "obsidianarmor";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/justin/obsidianarmor/ObsidianArmorClient.java
@@ -0,0 +1,10 @@
+package net.justin.obsidianarmor;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ObsidianArmorClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
